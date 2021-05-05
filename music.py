cd87e575de971b881a953e10e9513c2fc39d4bb1
a-llyson <67112226+a-llyson@users.noreply.github.com>
2021-05-05 19:17:28 -0400

    Plays music notes from password

--- music.py
+++ music.py
@@ -1,11 +1,21 @@
 from synthesizer import Player, Synthesizer, Waveform
+from password import password_gen, note_password_gen, convert_all
 
-player = Player()
-player.open_stream()
-synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
-'''
-notes = ['E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'D4', 'D4', 'D4', 'E4', 'G4', 'G4', 'E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'E4', 'D4', 'D4', 'E4', 'D4', 'C4']
-for note in notes:
-    player.play_wave(synthesizer.generate_constant_wave(note, 0.5))
-'''
-player.play_wave(synthesizer.generate_constant_wave('G#4', 1.0))
+x = password_gen(1, 15)
+y = note_password_gen(x)
+z = convert_all(y)
+print(z)
+
+def play_password(lop):
+    player = Player()
+    player.open_stream()
+    synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
+    '''
+    notes = ['E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'D4', 'D4', 'D4', 'E4', 'G4', 'G4', 'E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'E4', 'D4', 'D4', 'E4', 'D4', 'C4']
+    
+    '''
+    for password in lop:
+        for note in password:
+            player.play_wave(synthesizer.generate_constant_wave(note, 0.5))
+
+play_password(z)
--- password.py
+++ password.py
@@ -1,6 +1,11 @@
 import random
 import string
 
+notes = 'CDEFGAB1234567'
+pitch = '#b'
+octave_dot = 'oO' # upper case O is upper octave while lower case o is lower octave
+    # example: C#4bGBb17o  -> C# Fb G Bb C B(lower octave) -> notes should be played
+
 def note_convert(num):
     if num == '1':
         return 'C'
@@ -48,7 +53,7 @@ def converter(note):
         else:
             converted_note = val
 
-    if converted_note[-1] != '3' or converted_note[-1] != '5':
+    if converted_note[-1] != '3' and converted_note[-1] != '5':
         converted_note += '4'
     
     return converted_note
@@ -56,11 +61,6 @@ def converter(note):
 def password_gen(num_pass=1, pass_len=1):
     number_of_passwords = num_pass
     password_length = pass_len
-
-    notes = 'CDEFGAB1234567'
-    pitch = '#b'
-    octave_dot = 'oO' # upper case O is upper octave while lower case o is lower octave
-    # example: C#4bGBb17o  -> C# Fb G Bb C B(lower octave) -> notes should be played
 
     password_list = []
 
@@ -90,15 +90,16 @@ def password_gen(num_pass=1, pass_len=1):
         
         password_list.append(password)
 
+    return password_list
 
+def note_password_gen(pass_list):
     all_password_sound = []
     single_password_sound = []
 
     # separates password into notes
-    for i in range(number_of_passwords):
+    for i in pass_list:
         single_note = ''
-        new_note = True
-        for letter in password_list[i]:
+        for letter in i:
             if letter in notes:
                 if single_note != '':
                     single_password_sound.append(single_note)
@@ -114,7 +115,47 @@ def password_gen(num_pass=1, pass_len=1):
 
     return all_password_sound
 
+'''
+def note_password_gen(pass_list, num_pass=1, pass_len=1):
+    all_password_sound = []
+    single_password_sound = []
+    number_of_passwords = num_pass
+    password_list = pass_list
+    # separates password into notes
+    for i in range(number_of_passwords):
+        single_note = ''
+        for letter in password_list[i]:
+            if letter in notes:
+                if single_note != '':
+                    single_password_sound.append(single_note)
+                single_note = letter
+            else:
+                single_note += letter
+
+        if single_note != '':
+            single_password_sound.append(single_note)
+
+        all_password_sound.append(single_password_sound)    
+        single_password_sound = []
+
+    return all_password_sound
+'''
+
+def convert_all(list_of_passwords):
+    all_password_sound = []
+    single_password_sound = []
+    for i in list_of_passwords:
+        for k in i:
+            single_password_sound.append(converter(k))
+        
+        all_password_sound.append(single_password_sound)
+        single_password_sound = []
+    return all_password_sound
 
 
-print(password_gen(5, 7))
-print(converter('C#'))
+x = password_gen(2, 5)
+y = note_password_gen(x)
+z = convert_all(y)
+print(x)
+print(y)
+print(z)
