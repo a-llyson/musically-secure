b26784ed5019f97372f352e3f29e9d579ae0a0e0
a-llyson <67112226+a-llyson@users.noreply.github.com>
2021-05-06 16:43:21 -0400

    Writes to .wav files

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from password import password_gen, note_password_gen, convert_all 
 from music import play_password
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        print("hl")
+        number = request.form["name"]
+        length = request.form["length"]
+        list_of_passwords = password_gen(number, length)
+        list_of_notes = convert_all(note_password_gen(list_of_passwords))
+        return render_template("sound.html", list_of_passwords=list_of_passwords, length=length)
+    else:
+        return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- music.py
+++ music.py
@@ -1,23 +1,39 @@
-from synthesizer import Player, Synthesizer, Waveform
+from synthesizer import Player, Synthesizer, Waveform, Writer
 from password import password_gen, note_password_gen, convert_all
+import os
+import glob
+from pydub import AudioSegment
 
-'''
-x = password_gen(1, 15)
-y = note_password_gen(x)
-z = convert_all(y)
-#print(z)
-'''
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def play_password(lop):
+    i = 0
+    combined_sounds = AudioSegment.empty()
+    directory = "C:/Users/allys/Documents/Coding/vs/musically-secure"
+    combined = AudioSegment.empty()
+    writer = Writer()
     player = Player()
     player.open_stream()
     synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
-    '''
-    notes = ['E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'D4', 'D4', 'D4', 'E4', 'G4', 'G4', 'E4', 'D4', 'C4', 'D4', 'E4', 'E4', 'E4', 'E4', 'D4', 'D4', 'E4', 'D4', 'C4']
-    
-    '''
     for password in lop:
+        filename2 = "song" + str(i) + ".wav"
         for note in password:
-            player.play_wave(synthesizer.generate_constant_wave(note, 0.5))
+ #          player.synthesizer.generate_constant_wave(note, 0.5)
+            sound = synthesizer.generate_constant_wave(note, 0.5)
+            name = "test" + str(i) + ".wav"
+            i += 1
+            writer.write_waves(os.path.join(DIR, name), sound)
+        for file in sorted(os.listdir(directory)):
+            filename = os.path.join(directory,file)
+            if filename.endswith(".wav"):
+                p1 = AudioSegment.from_wav(filename)
+                combined_sounds += p1  
+        combined_sounds.export(filename2, format="wav")
 
-# play_password(z)
+
+x = password_gen(2, 7)
+y = note_password_gen(x)
+z = convert_all(y)
+play_password(z)
